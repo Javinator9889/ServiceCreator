a4552bac3d28311f6a45e701b20313fd387e989d
Javinator9889 <javialonso007@hotmail.es>
2018-05-13 23:08:07 +0200

    Version code & URLs modified

--- service_creator/values/Constants.py
+++ service_creator/values/Constants.py
@@ -5,7 +5,7 @@ MAIN_PROGRAM_NAME = "Service Creator"
 MAIN_PROGRAM_COMMAND = "service_creator"
 MAIN_PROGRAM_DESCRIPTION = "Create custom services for your Debian-based systems (such as Ubuntu) with this tool | " \
                            "Automate the process and make everything faster and easier than ever"
-MAIN_PROGRAM_VERSION = "0.1d"
+MAIN_PROGRAM_VERSION = "0.9b"
 MAIN_PROGRAM_EPILOG = MAIN_PROGRAM_NAME + " | v" + MAIN_PROGRAM_VERSION
 MAIN_PROGRAM_USAGE = Colors.HEADER + MAIN_PROGRAM_NAME + Colors.END_COLOR + "\nUse this tool for creating custom " \
                                                                             "init.d scripts that will run" \
@@ -23,10 +23,10 @@ MAIN_PROGRAM_USAGE = Colors.HEADER + MAIN_PROGRAM_NAME + Colors.END_COLOR + "\nU
 MAIN_PROGRAM_URL = "https://github.com/Javinator9889/ServiceCreator"
 
 # Other params
-OP_VERSION_RAW = "https://github.com/Javinator9889/ServiceCreator/raw/master/version.json"
+OP_VERSION_RAW = "https://github.com/Javinator9889/ServiceCreator/raw/development/version.json"
 OP_BASH_HEADER = "#!/bin/bash"
 OP_SH_HEADER = "#!/bin/bash"
-OP_TEMPLATE_FILE = "https://github.com/Javinator9889/ServiceCreator/raw/master/template.sh"
+OP_TEMPLATE_FILE = "https://raw.githubusercontent.com/Javinator9889/ServiceCreator/development/template.sh"
 
 # Paths
 P_ETC_INIT_DIR = "/etc/init.d/"
