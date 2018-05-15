ce8118ca66b66860e6554d9ff84b8b343f4bd4a5
Javinator9889 <javialonso007@hotmail.es>
2018-05-15 15:41:44 +0200

    Removed \n & \r from header

--- service_creator/utils/__init__.py
+++ service_creator/utils/__init__.py
@@ -79,6 +79,8 @@ def makeBashScript(filename: str, new_sh_file: str):
     with open(filename, 'r') as script:
         script_content = script.readlines()
     pprint(script_content)
+    pprint(script_content[0].rstrip())
+    pprint(script_content[0].strip())
     if (script_content[0].rstrip() != OP_BASH_HEADER) or (script_content[0].rstrip() != OP_SH_HEADER):
         script_content.insert(0, OP_SH_HEADER + "\n\n")
     usr_exec_file = os.path.basename(new_sh_file)
